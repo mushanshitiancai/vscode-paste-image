125cecd8929cc3c89174c24028adf5c156f2f123
mazhibin <mazhibin@ppzuche.com>
2016-08-21 22:39:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Paste Image
+
+Paste image directly from clipboard to markdown(or other file)!
+
+(Only for MacOS for now)
+
+## Usage
+
+1. capture screen to clipboard
+2. Open the command palette: `Ctrl+Shift+P` (`Cmd+Shift+P` on Mac)
+3. Type: Paste Image
+
+or you can use default keyboard binding: `Cmd+Alt+V`.
+
+when you editing a markdown, it will pasted as markdown image link format `![]()`, in other file, it just paste the image's path.
+
+## TODO
+
+- [ ] support config
+- [ ] support config relative/absolute path
+
+## License
+
+The extension and source are licensed under the [MIT license](LICENSE.txt).
---
+++ src/extension.ts
@@ -0,0 +1,95 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as path from 'path';
+import {spawn} from 'child_process';
+import * as moment from 'moment';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "vscode-paste-image" is now active!');
+
+    let disposable = vscode.commands.registerCommand('extension.pasteImage', () => {
+        Paster.paste();
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
+
+class Paster {
+
+    public static paste() {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+
+        let fileUri = editor.document.uri;
+        if (!fileUri) return;
+        if (fileUri.scheme === 'untitled') {
+            vscode.window.showInformationMessage('Before paste image, you need to save current edit file first.');
+        }
+
+        let filePath = fileUri.fsPath;
+        let imagePath = this.getImagePath(filePath);
+
+        // save image and insert to current edit file
+        this.saveClipboardImageToFileAndGetPath(imagePath,imagePath => {
+            if(imagePath === 'no image\n'){
+                vscode.window.showInformationMessage('There is not a image in clipboard.');
+                return;
+            }
+
+            imagePath = this.renderFilePath(editor.document.languageId,filePath,imagePath);
+
+            editor.edit(edit => {
+                let current = editor.selection;
+                
+                if(current.isEmpty){
+                    edit.insert(current.start,imagePath);
+                }else{
+                    edit.replace(current,imagePath);
+                }
+            })
+        });
+    }
+
+    public static getImagePath(filePath): string {
+        let folderPath = path.dirname(filePath);
+        let imageFileName = moment().format("Y-MM-DD-HH-mm-ss") + ".png";
+        let imageFilePath = path.join(folderPath, imageFileName);
+
+        return imageFilePath;
+    }
+
+
+    /**
+     * use applescript to save image from clipboard and get file path
+     */
+    private static saveClipboardImageToFileAndGetPath(imagePath,cb:(imagePath:string)=>void) {
+        if (!imagePath) return;
+        let scriptPath = path.join(__dirname, '../../src/util/mac.applescript');
+
+        let ascript = spawn('osascript', [scriptPath, imagePath]);
+        ascript.on('exit', function (code, signal) {
+            // console.log('exit',code,signal);
+        })
+
+        ascript.stdout.on('data', function (data:Buffer) {
+            cb(data.toString().trim());
+        })
+    }
+
+    /**
+     * render the image file path dependen on file type
+     * e.g. in markdown image file path will render to ![](path)
+     */
+    public static renderFilePath(languageId:string,docPath:string,imageFilePath:string):string{
+        imageFilePath = path.relative(path.dirname(docPath),imageFilePath);
+
+        if(languageId === 'markdown'){
+            return `![](${imageFilePath})`;
+        }else{
+            return imageFilePath;
+        }
+    }
+}
