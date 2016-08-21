4efae3c6ee9debf44e7682be60f2e54aa0f1488e
mazhibin <mazhibin@ppzuche.com>
2016-08-21 23:21:07 +0800

    fix no image bug

--- src/extension.ts
+++ src/extension.ts
@@ -34,7 +34,7 @@ class Paster {
 
         // save image and insert to current edit file
         this.saveClipboardImageToFileAndGetPath(imagePath,imagePath => {
-            if(imagePath === 'no image\n'){
+            if(imagePath === 'no image'){
                 vscode.window.showInformationMessage('There is not a image in clipboard.');
                 return;
             }
