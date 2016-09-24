0af756b10ea6d14e29a094f42260453e9573b0b0
mazhibin <mazhibin@ppzuche.com>
2016-09-24 11:35:53 +0800

    add support to win/linux

---
+++ res/electron-app/main.js
@@ -0,0 +1,31 @@
+const {app, clipboard, BrowserWindow} = require('electron')
+const fs = require("fs");
+
+app.on('ready', saveClipboard)
+
+function saveClipboard(){
+    var imagePath = process.argv[2];
+    log("imagePath: "+imagePath);
+
+    var image = clipboard.readImage();
+    if(!image || image.isEmpty()) 
+        return quitWithResult("no image");
+    image = image.toPNG();
+
+    fs.writeFile(imagePath,image,function(err){
+        if(err){
+            quitWithResult("save fail");
+        }else{
+            quitWithResult(imagePath);
+        }
+    });
+}
+
+function quitWithResult(msg){
+    console.log(msg);
+    app.quit();
+}
+
+function log(msg){
+    console.error(msg);
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,10 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import {spawn} from 'child_process';
 import * as moment from 'moment';
+const electron = require('electron');
+import * as child_process from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "vscode-paste-image" is now active!');
-
     let disposable = vscode.commands.registerCommand('extension.pasteImage', () => {
         Paster.paste();
     });
@@ -38,6 +38,10 @@ class Paster {
                 vscode.window.showInformationMessage('There is not a image in clipboard.');
                 return;
             }
+            if(imagePath === 'save fail'){
+                vscode.window.showInformationMessage('Save image fail.');
+                return;
+            }
 
             imagePath = this.renderFilePath(editor.document.languageId,filePath,imagePath);
 
@@ -67,16 +71,26 @@ class Paster {
      */
     private static saveClipboardImageToFileAndGetPath(imagePath,cb:(imagePath:string)=>void) {
         if (!imagePath) return;
-        let scriptPath = path.join(__dirname, '../../res/mac.applescript');
 
-        let ascript = spawn('osascript', [scriptPath, imagePath]);
-        ascript.on('exit', function (code, signal) {
-            // console.log('exit',code,signal);
-        })
+        if(process.platform !== 'darwin'){
+            let scriptPath = path.join(__dirname, '../../res/mac.applescript');
 
-        ascript.stdout.on('data', function (data:Buffer) {
-            cb(data.toString().trim());
-        })
+            let ascript = spawn('osascript', [scriptPath, imagePath]);
+            ascript.on('exit', function (code, signal) {
+                // console.log('exit',code,signal);
+            })
+
+            ascript.stdout.on('data', function (data:Buffer) {
+                cb(data.toString().trim());
+            })
+        }else{
+            let electronAppPath = path.join(__dirname, '../../res/electron-app');
+            var proc = child_process.spawn(electron,[electronAppPath,imagePath]);
+            proc.stdout.on("data",function(data){
+                cb(data.toString().trim());
+            })
+            proc.on("close",function(){});
+        }
     }
 
     /**
