21caad280ecbb20d0e97a0113a431e2150ea09b9
sakamoto <sakamoto0321@gmail.com>
2020-08-26 16:11:07 +0900

    fix linux.sh, mac.sh

--- res/mac.sh
+++ res/mac.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 # require xclip(see http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script/677212#677212)
-command -v pngpaste >/dev/null 2>&1 || { echo >&1 "no pngpaste"; exit 1; }
+command -v pngpaste >/dev/null 2>&1 || { echo >&1 "no pngpaste"; exit 0; }
 
 # write image in clipboard to file (see http://unix.stackexchange.com/questions/145131/copy-image-from-clipboard-to-file)
 if
 pngpaste - >/dev/null 2>&1
 then
-pngpaste - >$1 2>/dev/null
-echo -n $1 | base64
+pngpaste - 2>/dev/null | base64
 else
 echo "no image"
 fi
