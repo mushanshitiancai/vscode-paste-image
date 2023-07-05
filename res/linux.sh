f477ea10d51dc87684126bf59de3edf24d2f9324
Dennis Værum <6872940+dvaerum@users.noreply.github.com>
2023-07-05 12:28:21 +0200

    Added support for wayland (Linux)
    `xclip` does not support wayland, so if xclip is not install the program should check for the `wl-paste` (part of "wl-clipboard" package) and use that instead. Hopefully this will be useful for others 😊

--- res/linux.sh
+++ res/linux.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 # require xclip(see http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script/677212#677212)
-command -v xclip >/dev/null 2>&1 || { echo >&1 "no xclip"; exit 1; }
-
-# write image in clipboard to file (see http://unix.stackexchange.com/questions/145131/copy-image-from-clipboard-to-file)
-if
-xclip -selection clipboard -target image/png -o >/dev/null 2>&1
-then
-xclip -selection clipboard -target image/png -o >$1 2>/dev/null
-echo $1
+if command -v xclip >/dev/null 2>&1; then
+    # write image in clipboard to file (see http://unix.stackexchange.com/questions/145131/copy-image-from-clipboard-to-file)
+    if xclip -selection clipboard -target image/png -o >/dev/null 2>&1; then
+        xclip -selection clipboard -target image/png -o >"$1" 2>/dev/null
+        echo "$1"
+    else
+        echo "no image"
+    fi
+elif command -v wl-paste >/dev/null 2>&1; then
+    if wl-paste --type image/png >/dev/null 2>&1; then
+        wl-paste --type image/png >"$1" 2>/dev/null
+        echo "$1"
+    else
+        echo "no image"
+    fi
 else
-echo "no image"
+    echo >&1 "no xclip or wl-paste (wl-clipboard)"
+    exit 1
 fi
